45d63d85080f7b2b2460f612e93a667fd31321fe
Julie Kuehl <julie@juliekuehl.com>
2015-05-20 12:23:45 -0500

    content area styling

--- wp-content/themes/partyonstillwater/header.php
+++ wp-content/themes/partyonstillwater/header.php
@@ -20,23 +20,26 @@
 <body <?php body_class(); ?>>
 
 <div id="swoop"></div>
-<div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'partyonstillwater' ); ?></a>
-<!--<div id="wrapper">-->
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Party-On-Stillwater-logo_tagline.png" alt="Party on Stillwater: Life is short. Party on!" />
-			<div class="social-media-links">
-				<a href="http://facebook.com"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/facebook.png" alt="Facebook" /></a>
-				<a href="http://twitter.com"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/twitter.png" alt="Twitter" /></a>
-				<a href="http://pinterest.com"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pinterest.png" alt="Pinterest" /></a>
-			</div><!-- .social-media-links -->
-		</div><!-- .site-branding -->
-		<div class="weddings-site">
-			<a href="http://partyonstillwater.com/weddings"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/PartyOnWeddings_Nav-tab.png" alt="Party on Weddings logo" /></a>
-		</div><!-- .weddings-site -->
-	</header><!-- #masthead -->
+<div id="wrapper" class="page-header">
+	<div id="page" class="hfeed site">
+	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'partyonstillwater' ); ?></a>
+
+		<header id="masthead" class="site-header" role="banner">
+			<div class="site-branding">
+				<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/Party-On-Stillwater-logo_tagline.png" alt="Party on Stillwater: Life is short. Party on!" />
+				<div class="social-media-links">
+					<a href="http://facebook.com"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/facebook.png" alt="Facebook" /></a>
+					<a href="http://twitter.com"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/twitter.png" alt="Twitter" /></a>
+					<a href="http://pinterest.com"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/pinterest.png" alt="Pinterest" /></a>
+				</div><!-- .social-media-links -->
+			</div><!-- .site-branding -->
+			<div class="weddings-site">
+				<a href="http://partyonstillwater.com/weddings"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/PartyOnWeddings_Nav-tab.png" alt="Party on Weddings logo" /></a>
+			</div><!-- .weddings-site -->
+		</header><!-- #masthead -->
+	</div><!-- #page -->
+</div><!-- #wrapper -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'partyonstillwater' ); ?></button>
